b211d46e6d88cb857d86bf883d420c97dd02f646
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-03-12 22:24:08 +0800

    Lab 2

--- index.ts
+++ index.ts
@@ -64,7 +64,34 @@ const sketch = function (p: p5) {
 			p.line(this.x, this.y, that.x, that.y);
 		}
 
-		slopeTo(that: Point): number {}
+		slopeTo(that: Point): number {
+			if (this.x === that.x && this.y === that.y) {
+				return Number.NEGATIVE_INFINITY;
+			}
+			if (this.y === that.y) {
+				return 0;
+			}
+			if (this.x === that.x) {
+				return Number.POSITIVE_INFINITY;
+			}
+			return (that.y - this.y) / (that.x - this.x);
+		}
+
+		compareTo(that: Point): number {
+			if (this.y < that.y) {
+				return -1;
+			}
+			if (this.y > that.y) {
+				return 1;
+			}
+			if (this.x < that.x) {
+				return -1;
+			}
+			if (this.x > that.x) {
+				return 1;
+			}
+			return 0;
+		}
 	}
 
 	class LineSegment {
