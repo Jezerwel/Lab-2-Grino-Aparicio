3cc8d78cb0b2e8f33b76a04cc75da34b100f3d94
Jezerwel <jezerwel.grino-22@cpu.edu.ph>
2024-03-12 22:18:19 +0800

    lab 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import p5 from "p5";
-import Point from "./point";
 
 const width: number = 800;
 const height: number = 500;
 const padding: number = 50;
 
-const sketch = function (p) {
+const sketch = function (p: p5) {
 	// biome-ignore lint/complexity/useArrowFunction: <explanation>
 	p.setup = function () {
 		p.createCanvas(width, height);
@@ -93,7 +92,55 @@ const sketch = function (p) {
 			return `${this.p} -> ${this.q}`;
 		}
 	}
+	function mergeSort(points: Point[], origin: Point): Point[] {
+		if (points.length <= 1) {
+			return points;
+		}
 
+		const middle = Math.floor(points.length / 2);
+		const left = points.slice(0, middle);
+		const right = points.slice(middle);
+
+		return merge(mergeSort(left, origin), mergeSort(right, origin), origin);
+	}
+
+	function merge(left: Point[], right: Point[], origin: Point): Point[] {
+		let result: Point[] = [];
+		let leftIndex = 0;
+		let rightIndex = 0;
+
+		while (leftIndex < left.length && rightIndex < right.length) {
+			const slopeLeft = origin.slopeTo(left[leftIndex]);
+			const slopeRight = origin.slopeTo(right[rightIndex]);
+
+			if (slopeLeft < slopeRight) {
+				result.push(left[leftIndex]);
+				leftIndex++;
+			} else if (slopeLeft > slopeRight) {
+				result.push(right[rightIndex]);
+				rightIndex++;
+			} else {
+				const sameSlope = [left[leftIndex], right[rightIndex]];
+				while (
+					leftIndex < left.length &&
+					origin.slopeTo(left[leftIndex]) === slopeLeft
+				) {
+					sameSlope.push(left[leftIndex]);
+					leftIndex++;
+				}
+				while (
+					rightIndex < right.length &&
+					origin.slopeTo(right[rightIndex]) === slopeRight
+				) {
+					sameSlope.push(right[rightIndex]);
+					rightIndex++;
+				}
+				result = result.concat(sameSlope.sort((a, b) => a.compareTo(b)));
+			}
+		}
+
+		return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+	}
 	class BruteCollinearPoints {
 		points: Point[];
 		lineSegments: Set<LineSegment>;
@@ -220,7 +267,12 @@ const sketch = function (p) {
 		}
 
 		const collinear = new FastCollinearPoints(points);
+		const collinear2 = new BruteCollinearPoints(points);
 		for (const segment of collinear.segments()) {
+			console.log(segment.toString());
+			segment.draw();
+		}
+		for (const segment of collinear2.segments()) {
 			console.log(segment.toString());
 			segment.draw();
 		}
